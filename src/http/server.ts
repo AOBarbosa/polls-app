507e543a19e7fd488e1e7e7ba900fef0152c0b55
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-02-08 00:02:23 -0300

    App working

---
+++ src/http/server.ts
@@ -0,0 +1,23 @@
+import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-polls'
+import { voteOnPoll } from './routes/vote-on-poll'
+
+const app = fastify()
+
+app.register(cookie, {
+  secret: "polls-app-nlw",
+  hook: 'onRequest',
+})
+
+app.register(websocket)
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('🚀 HTTP server running')
+}) 
